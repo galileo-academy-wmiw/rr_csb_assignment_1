16a6015e1ab49d6a59e3e3738b7d9710b872163c
Robin Rahman <robin.rahman.nl@gmail.com>
2026-01-12 10:59:43 +0100

    Chore: Update docs

--- Program.cs
+++ Program.cs
@@ -4,19 +4,15 @@ using BibliotheekbeheerSysteem;
 Book book1 = new Book("Foundation", "Isaac Asimov", "9780553293357", 1951, "sifi");
 Book book2 = new Book("Dune", "Frank Herbert", "9780441172719", 1965, "sifi");
 Book book3 = new Book("1984", "George Orwell", "9780451524935", 1949, "sifi");
+Book Ebook1 = new Ebook("Neuromancer", "William Gibson", "9780441569595", 1984, "sci-fi", 10);
 
 // Test AddBook()
 Library library1 = new Library();
 library1.AddBook(book1);
 library1.AddBook(book2);
 library1.AddBook(book3);
+library1.AddBook(Ebook1);
 
-// Show all books
-// library1.ListBooks();
-
-// Test SearchBooksbyTitle(string title)
-// Console.WriteLine(library1.SearchBooks("1984"));
-
-List<Book> searchResult = library1.SearchBooks("1984");
-library1.prettify(searchResult);
-book1.Read();
+// UI class
+UI.DisplayMenu();
+UI.GetUserInput();
