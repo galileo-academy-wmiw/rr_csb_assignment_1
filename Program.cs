57d9500e6f285bc8635fdb678891fc74c7293215
Robin Rahman <robin.rahman.nl@gmail.com>
2026-01-22 12:20:02 +0100

    Correct typo ebook genre sci-fii

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ using BibliotheekbeheerSysteem;
 Book book1 = new Book("Foundation", "Isaac Asimov", "9780553293357", 1951, "sifi");
 Book book2 = new Book("Dune", "Frank Herbert", "9780441172719", 1965, "sifi");
 Book book3 = new Book("1984", "George Orwell", "9780451524935", 1949, "sifi");
-Book Ebook1 = new Ebook("Neuromancer", "William Gibson", "9780441569595", 1984, "sci-fi", 10);
+Book Ebook1 = new Ebook("Neuromancer", "William Gibson", "9780441569595", 1984, "sifi", 10);
 
 // Add books
 Library libraryRotterdam = new Library();
